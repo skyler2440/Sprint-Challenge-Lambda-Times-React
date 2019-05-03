b9f1840bd6f00fbb3f242633704426a4ea1fecd8
Skyler Dowdy <skyler2440@gmail.com>
2019-05-03 13:49:50 -0400

    fixed img src NTA Proptypes

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -7,7 +7,7 @@ const Card = props => {
       <div className="headline">{props.card.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={props.card.image} alt='card image' />
+          <img src={props.card.image} alt=''/>
         </div>
         <span>By {props.card.author}</span>
       </div>
